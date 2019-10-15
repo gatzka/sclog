eabc1cc1c9406318377fc1f50f3cd79b07064d7c
Stephan Gatzka <stephan.gatzka@hbm.com>
2019-10-15 09:08:41 +0200

    Add structure fo file rotate sink.

---
+++ src/sclog_file_rotate.h
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) <2019> <Stephan Gatzka>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SC_LOG_FILE_ROTATE_H
+#define SC_LOG_FILE_ROTATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "sclog.h"
+#include "sclog_export.h"
+
+struct sc_log_file_rotate_sink {
+	struct sc_log_sink sink;
+	const char *log_file_name;
+	unsigned int number_of_files;
+	long single_file_size;
+};
+
+SCLOG_EXPORT int sc_log_file_rotate_sink_init(struct sc_log_file_rotate_sink *fr_sink);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
