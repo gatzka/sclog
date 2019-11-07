3d16187cbdfcbe33b60402a8b77a7f7e27c6a089
Stephan Gatzka <stephan.gatzka@hbm.com>
2019-11-07 15:22:32 +0100

    Add scricpt to push documentation to github pages.

---
+++ scripts/push-doxygen-to-gh-pages.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "gatzka/sclog" ] && [ "$CREATE_DOXY" = "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Publishing doxygen...\n"
+
+  cp -R /tmp/sclog/src/docs/html/ $HOME/doxygen-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/gatzka/sclog gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./doc
+  cp -Rf $HOME/doxygen-latest ./doc
+  git add -f .
+  git commit -m "Latest doxygen on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published doxygen to gh-pages.\n"
+  
+fi
