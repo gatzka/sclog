c5ceb37a618566c2b7a7af191827898fac9b4c92
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-11-17 12:11:08 +0100

    Add parameter for installing dependencies.

--- .github/actions/ctest-build/entrypoint.sh
+++ .github/actions/ctest-build/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 set -eu
-echo "Hello action"
-ls -l
+
+if [ "x$INPUT_INSTALL_DEPS" != 'x' ]; then
+  echo "got dependency script: $INPUT_INSTALL_DEPS"
+else
+  echo "no dependency script"
+fi
+
 ctest -VV -S build.cmake
