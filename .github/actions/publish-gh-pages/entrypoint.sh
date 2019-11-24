c553c80b67428ceb3f1288197cd283c602731ddd
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-11-24 16:42:18 +0100

    Take outd ebug output

--- .github/actions/publish-gh-pages/entrypoint.sh
+++ .github/actions/publish-gh-pages/entrypoint.sh
@@ -17,24 +17,18 @@ cd ${TMP_DIR} && \
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-pwd && \
-echo ${REMOTE_REPO} && \
 git clone --branch=gh-pages ${REMOTE_REPO} gh-pages && \
-echo "here 1" && \
 cd gh-pages && \
 if [ -d "${GH_PAGES_SUBDIR}" ]; then
   git rm -rf ${GH_PAGES_SUBDIR}
 fi && \
 mkdir ${GH_PAGES_SUBDIR} && \
 cp -Rf ${GITHUB_WORKSPACE}/${BUILD_DIR}/* ${GH_PAGES_SUBDIR} && \
-echo "here 2" && \
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
 git add -f . && \
 git commit -m 'Deploy to GitHub Pages' && \
-echo "here 3" && \
 git push -fq origin gh-pages && \
 rm -fr .git && \
-echo "here 4" && \
 cd $GITHUB_WORKSPACE && \
 echo "Content of $BUILD_DIR has been deployed to GitHub Pages."
