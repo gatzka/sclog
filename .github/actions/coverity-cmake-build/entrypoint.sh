59bf1abf6ad09564448f586e9d8398d161a5bf31
Stephan Gatzka <stephan.gatzka@gmail.com>
2019-12-01 12:37:30 +0100

    Add cov-build to path.

--- .github/actions/coverity-cmake-build/entrypoint.sh
+++ .github/actions/coverity-cmake-build/entrypoint.sh
@@ -14,8 +14,13 @@ fi
 CMAKE_OPTIONS+=${INPUT_CMAKE_BUILD_TYPE}
 
 wget --quiet https://scan.coverity.com/download/linux64 --post-data "token=${INPUT_COVERITY_TOKEN}&project=gatzka%2Fsclog" -O coverity_tool.tgz
+tar -xf coverity_tool.tgz
+cd cov-analysis-linux64*
+export PATH=$PATH:`pwd`/bin
+cd ..
 mkdir build
 cd build
+cov-build
 
 cmake .. ${CMAKE_OPTIONS}
 cmake --build . ${CMAKE_BUILD_TARGET}
